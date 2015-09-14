fd13232e0bf73eb697d1a32f0edc7bb39080230b
Valdemar Roxling <valdemar.roxling@gmail.com>
2015-09-14 12:07:48 +0200

    linking with glew

--- Smegod/Smegod/Core.cpp
+++ Smegod/Smegod/Core.cpp
@@ -1,35 +1,43 @@
-#include <GLFW/glfw3.h>
+#include <memory>
+#include <iostream>
 
-int main(void)
-{
-	GLFWwindow* window;
+#include <GL\glew.h>
+#include <GLFW\glfw3.h>
 
-	/* Initialize the library */
-	if (!glfwInit())
+using namespace std;
+
+const int width = 800;
+const int height = 600;
+const string name = "Window";
+
+int main() {
+	if (!glfwInit()) {
+		cout << "Failed to initialize GLFW" << endl;
 		return -1;
+	}
+	GLFWwindow *window(glfwCreateWindow(width, height, name.c_str(), NULL, NULL));
 
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-	if (!window)
-	{
+	if (!window) {
+		cout << "Failed to create window" << endl;
 		glfwTerminate();
 		return -1;
 	}
 
-	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
+	glewExperimental = true;
 
-	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(window))
-	{
-		/* Render here */
-
-		/* Swap front and back buffers */
-		glfwSwapBuffers(window);
-
-		/* Poll for and process events */
-		glfwPollEvents();
+	if (glewInit() != GLEW_OK) {
+		cout << "Failed to initialize GLEW" << endl;
+		return -1;
 	}
+
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	do {
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	} while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && !glfwWindowShouldClose(window));
 
 	glfwTerminate();
 	return 0;

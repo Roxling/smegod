6f27d5ffc29d346ded9f3ffd23c8d460ee5db490
Valdemar Roxling <valdemar.roxling@gmail.com>
2015-09-25 09:48:54 +0200

    textures now working with light

--- Smegod/Smegod/Shaders/phong.fs
+++ Smegod/Smegod/Shaders/phong.fs
@@ -16,14 +16,14 @@ in vec3 fV;
 
 void main()
 {
-    //color = (2 * texture(tex, tex_coord) + vec4(incolor, 1.0f))/3;
 
     vec3 N = normalize(fN);
     vec3 L = normalize(fL);
     vec3 V = normalize(fV);
     vec3 R = normalize(reflect(-L,N));
-    vec3 diffuse = kdiffuse*max(dot(L,N),0.0);
+    vec3 diffuse = texture(tex, tex_coord).xyz*max(dot(L,N),0.0);
     vec3 specular = kspecular*pow(max(dot(V,R),0.0), shininess);
     color.xyz = kambient + diffuse + specular;
     color.w = 1.0; 
+    //color = (2 * texture(tex, tex_coord) + vec4(incolor, 1.0f))/3;
 } 
